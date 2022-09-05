da296ed6a75a82a83b3488257984771b734ea32e
shizhengkang <77472773+shizhengkang@users.noreply.github.com>
2022-09-05 20:45:22 +0800

    Update airwar_3D.py

--- airwar_3D.py
+++ airwar_3D.py
@@ -7,16 +7,25 @@ import numpy as np
 from Agent import UCAVs
 import math
 import pygame
+import matplotlib.pyplot as plt
+from Model_Render import flight_model, missile_model
 
 
 def distance(x1, x2):
     return math.sqrt(sum((x1 - x2) * (x1 - x2)))
 
 
+def unitization(a):
+    try:
+        return a / math.sqrt(sum(a * a))
+    except:
+        return a
+
+
 class AirWarEnv3D(gym.Env):
     def __init__(self):
-        self.grid_size = 150
-        self.agent_number = 2
+        self.grid_size = 50
+        self.agent_number = 1
         self.agent_ctrl = []
         self.agent_oppo = []
         for i in range(self.agent_number):
@@ -34,25 +43,28 @@ class AirWarEnv3D(gym.Env):
 
     def step(self, action):
         """
-        action是一个2*4维输入列表，其中：
+        action是一个2*self.agent_number*4维输入列表，其中：
         action[0]与action[1]分别为博弈双方的动作，
-        action[:][0]表示速度的增减，取值范围为[]
-        action[:][1]表示俯仰角的改变，取值范围为[]
-        action[:][2]表示滚转角的改变，取值范围为[]
-        action[:][3]表示是否发射炮弹，取值范围为{0, 1}
+        action[:][j]表示某个阵营第j个智能体的动作，
+        action[:][:][0]表示速度的增减，取值范围为[]
+        action[:][:][1]表示俯仰角的改变，取值范围为[]
+        action[:][:][2]表示滚转角的改变，取值范围为[]
+        action[:][:][3]表示是否发射炮弹，取值范围为{0, 1}
         """
 
         for i in range(2):
             for j in range(self.agent_number):
                 if self.agents[i][j].alive:
-                    self.agents[i][j].velocity_change(action[i][0])
-                    self.agents[i][j].pitch(action[i][1])
-                    self.agents[i][j].roll(action[i][2])
+                    self.agents[i][j].velocity_change(action[i][j][0])
+                    self.agents[i][j].pitch(action[i][j][1])
+                    self.agents[i][j].roll(action[i][j][2])
                     self.agents[i][j].move()
                     self.agents[i][j].oil_change()
-                    self.agents[i][j].launch(action[i][3])
-                for k in range(self.agents[i][j].mis_num):
+                    self.agents[i][j].launch(action[i][j][3])
+                k = 0
+                while k < len(self.agents[i][j].mis):
                     self.track(i, j, k)
+                    k += 1
 
         result, done = self.is_terminal()
 
@@ -73,8 +85,29 @@ class AirWarEnv3D(gym.Env):
         return self.state
 
     def render(self, mode='human'):
+        if self.viewer is None:
+            self.viewer = plt.axes(projection='3d')
+            self.viewer.set_xlim((0, self.grid_size))
+            self.viewer.set_ylim((0, self.grid_size))
+            self.viewer.set_zlim((0, self.grid_size))
+            plt.ion()
 
-        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+        objects = []
+        color = ['b', 'r']
+        for i in range(2):
+            for j in range(self.agent_number):
+                if self.agents[i][j].alive:
+                    point = flight_model(self.agents[i][j].pos, self.agents[i][j].tow, self.agents[i][j].nor)
+                    flight, = self.viewer.plot3D(point[0], point[1], point[2], c=color[i], linewidth=0.5)
+                    objects.append(flight)
+                for k in range(len(self.agents[i][j].mis)):
+                    point = missile_model(self.agents[i][j].mis[k].pos, self.agents[i][j].mis[k].tow)
+                    missile, = self.viewer.plot3D(point[0], point[1], point[2], c='black', linewidth=0.5)
+                    objects.append(missile)
+        plt.show()
+        plt.pause(0.01)
+        for object_ in objects:
+            object_.remove()
 
     def track(self, i, j, k):
         index = None
@@ -88,10 +121,11 @@ class AirWarEnv3D(gym.Env):
                     min_distance = distance
                     direction = direc
                     index = n
-        self.agents[i][j].mis[k].update(direction)
         if min_distance < self.agents[i][j].mis[k].blast_range:
             self.agents[1-i][index].alive = 0
             del self.agents[i][j].mis[k]
+        else:
+            self.agents[i][j].mis[k].update(direction)
 
     def is_terminal(self):
         if self.agent_remain[0] != 0 and self.agent_remain[1] == 0:
@@ -120,9 +154,10 @@ class AirWarEnv3D(gym.Env):
                     o = []
                     o += (self.agents[i][j].pos / self.grid_size).tolist()
                     o += self.agents[i][j].tow.tolist()
+                    o += self.agents[i][j].nor.tolist()
                     o += [self.agents[i][j].vel / 100.]
                     o += [self.agents[i][j].oil / 100.]
-                    o += [self.agents[i][j].mis_num / self.agent_ctrl[i].init_mis_num]
+                    o += [self.agents[i][j].mis_num / self.agent_ctrl[j].init_mis_num]
                     for k in range(self.agents[i][j].init_mis_num):
                         try:
                             o += (self.agents[i][j].mis[k].pos / self.grid_size).tolist()
@@ -136,9 +171,10 @@ class AirWarEnv3D(gym.Env):
                         if k != j:
                             o += (self.agents[i][k].pos / self.grid_size).tolist()
                             o += self.agents[i][k].tow.tolist()
+                            o += self.agents[i][k].nor.tolist()
                             o += [self.agents[i][k].vel / 100.]
                             o += [self.agents[i][k].oil / 100.]
-                            o += [self.agents[i][k].mis_num / self.agent_ctrl[i].init_mis_num]
+                            o += [self.agents[i][k].mis_num / self.agent_ctrl[k].init_mis_num]
                             for n in range(self.agents[i][k].init_mis_num):
                                 try:
                                     o += (self.agents[i][k].mis[n].pos / self.grid_size).tolist()
@@ -174,9 +210,10 @@ class AirWarEnv3D(gym.Env):
                 if self.agents[i][j].alive:
                     states += (self.agents[i][j].pos / self.grid_size).tolist()
                     states += self.agents[i][j].tow.tolist()
+                    states += self.agents[i][j].nor.tolist()
                     states += [self.agents[i][j].vel / 100.]
                     states += [self.agents[i][j].oil / 100.]
-                    states += [self.agents[i][j].mis_num / self.agent_ctrl[i].init_mis_num]
+                    states += [self.agents[i][j].mis_num / self.agent_ctrl[j].init_mis_num]
                 else:
                     states += [0.] * 9
                 for k in range(self.agents[i][j].init_mis_num):
@@ -194,5 +231,5 @@ class AirWarEnv3D(gym.Env):
 
     def close(self):
         if self.viewer:
-            self.viewer.close()
+            plt.close()
             self.viewer = None
